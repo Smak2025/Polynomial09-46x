98fe064ef25dc5dbcd8c90a925e56b9f887027f4
Smak2025 <smak-80+2025@yandex.ru>
2026-02-13 13:41:07 +0300

    Polynomial toString()

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,14 @@
+import ru.gr0946x.Polynomial;
+
 void main() {
-  $END$
+    Polynomial p = new Polynomial(1, 2, 3);
+    Polynomial p2 = new Polynomial(1, -2, -3);
+    Polynomial p3 = new Polynomial(1, 0, -3);
+    Polynomial p4 = new Polynomial(0, 0, -3);
+    Polynomial p5 = new Polynomial(0, -1, -3);
+    System.out.println(p);
+    System.out.println(p2);
+    System.out.println(p3);
+    System.out.println(p4);
+    System.out.println(p5);
 }
--- src/ru/gr0946x/Polynomial.java
+++ src/ru/gr0946x/Polynomial.java
@@ -1,4 +1,65 @@
 package ru.gr0946x;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
 public class Polynomial {
+    private Map<Integer, Double> coeffs;
+
+    public Polynomial(){
+        coeffs = new TreeMap<>(
+                (k1, k2) -> -k1.compareTo(k2)
+        );
+        coeffs.put(0, 0.0);
+    }
+
+    public Polynomial(Map<Integer, Double> coeffs){
+        this();
+        this.coeffs.clear();
+        this.coeffs.putAll(coeffs);
+        filterCoeffs();
+    }
+
+    public Polynomial(double... coeffs){
+        this();
+        for(int i = 0; i < coeffs.length; i++){
+            this.coeffs.put(i, coeffs[i]);
+        }
+        filterCoeffs();
+    }
+
+    private void filterCoeffs(){
+        coeffs.entrySet().removeIf(
+                entry ->
+                        entry.getValue() == 0
+                                ||entry.getKey() < 0
+        );
+        if (coeffs.isEmpty()) coeffs.put(0, 0.0);
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        for (var entry : coeffs.entrySet()){
+            if (entry.getValue() >= 0) {
+                if (i != 0) sb.append("+");
+            } else {
+                sb.append("-");
+            }
+            if (Math.abs(entry.getValue()) != 1.0
+                || entry.getKey() == 0
+            ) {
+                sb.append(Math.abs(entry.getValue()));
+            }
+            if (entry.getKey() != 0) sb.append("x");
+            if (entry.getKey() > 1) {
+                sb.append("^").append(entry.getKey());
+            }
+            i++;
+        }
+        return sb.toString();
+    }
 }
