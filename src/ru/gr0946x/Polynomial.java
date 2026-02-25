054c6b934034fcf7e619b9aad4e1cba61bb4c91a
Smak2025 <smak-80+2025@yandex.ru>
2026-02-25 18:21:01 +0300

    Полином Лагранжа

--- src/ru/gr0946x/Polynomial.java
+++ src/ru/gr0946x/Polynomial.java
@@ -4,7 +4,7 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class Polynomial {
-    private final Map<Integer, Double> coeffs;
+    protected final Map<Integer, Double> coeffs;
 
     public Polynomial(){
         coeffs = new TreeMap<>(
